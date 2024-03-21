2a3653915af7b3b9d0ee2d86790e471e1119cef7
Junaith1994 <daya2018ctg@gmail.com>
2024-03-22 03:26:05 +0600

    Modified googleChart data, Modified last7dDaysProducts section

--- src/Pages/DeliveryQuantityChart/DeliveryQtyChart.jsx
+++ src/Pages/DeliveryQuantityChart/DeliveryQtyChart.jsx
@@ -7,7 +7,7 @@ const DeliveryQtyChart = () => {
     const [productNames, setProductNames] = useState([]);
     const [deliveredQty, setDeleveredQty] = useState([]);
     const [date, setDate] = useState([]);
-    const [stockQty, setStockQty] = useState(0);
+    const [stockQty, setStockQty] = useState([]);
 
 
     useEffect(() => {
@@ -16,6 +16,7 @@ const DeliveryQtyChart = () => {
         const newDeliveryQty = [];
         const newDates = [];
 
+        // Extracting product names, stockedQty, deliveredQty and dates and pushing these into new array 
         products.map(product => {
             newNames.push(product.itemName);
             newStockQty.push(parseInt(product.quantity));
@@ -29,33 +30,18 @@ const DeliveryQtyChart = () => {
 
     }, [products])
 
-    // console.log('Product Name:',productNames.length, 'Stock-Qty:', stockQty.length, 'Date:', date.length);
+    // console.log('Product Name:', productNames.length, 'Stock-Qty:', stockQty.length, 'Date:', date.length);
+    // console.log('Product Name:', productNames, 'Stock-Qty:', stockQty, 'Date:', date);
     // console.log(date);
 
     const data = [
-        productNames,
-        // date,
-        // date,
-        deliveredQty,
+        ['Date', 'StockedQty', 'DeleveredQty'],
+        ...productNames.map((productName, index) => [
+            date[index],
+            stockQty[index],
+            deliveredQty[index],
+        ])
     ];
-
-    // const data = [
-    //     [
-    //         "Month",
-    //         "Bolivia",
-    //         "Ecuador",
-    //         "Madagascar",
-    //         "Papua New Guinea",
-    //         "Rwanda",
-    //         "Average",
-    //     ],
-    //     ["2004/05", 165, 938, 522, 998, 450, 614.6],
-    //     ["2005/06", 135, 1120, 599, 1268, 288, 682],
-    //     ["2006/07", 157, 1167, 587, 807, 397, 623],
-    //     ["2007/08", 139, 1110, 615, 968, 215, 609.4],
-    //     ["2008/09", 136, 691, 629, 1026, 366, 569.6],
-    // ];
-
     const options = {
         title: "Stocked Quantity and Delivery Chart",
         vAxis: { title: "Stocked Qty" },
--- src/Pages/Last7DaysProducts/Last7DaysProducts.jsx
+++ src/Pages/Last7DaysProducts/Last7DaysProducts.jsx
@@ -4,6 +4,7 @@ import AddedItem from "./AddedItem";
 
 const Last7DaysProducts = () => {
     const [addedProducts, setAddedProducts] = useState([]);
+    // console.log('AddedProducts:', addedProducts);
 
     useEffect(() => {
         axios.get('https://jk-elite-securities-warehouse-server.vercel.app/recently-added')
@@ -16,14 +17,20 @@ const Last7DaysProducts = () => {
     return (
         <div className="container mx-auto">
             <h1 className="text-4xl text-center my-5 text-fuchsia-950 font-bold">Added Products In Last 7 Days</h1>
-            <div className="grid md:grid-cols-3 grid-cols-2 gap-3">
-                {
-                    addedProducts.map(item => <AddedItem
-                        key={item._id}
-                        item={item}
-                    ></AddedItem>)
-                }
-            </div>
+            {
+                addedProducts.length !== 0 ? <div className="grid md:grid-cols-3 grid-cols-2 gap-3">
+                    {
+                        addedProducts.map(item => <AddedItem
+                            key={item._id}
+                            item={item}
+                        ></AddedItem>)
+                    }
+                </div>
+                    :
+                    <div>
+                        <h2 className="text-3xl text-center text-red-600 font-semibold">No products added in last 7 days</h2>
+                    </div>
+            }
             {/* <div className="text-center my-5">
                 <button onClick={() => navigate("/manage-products")} className="bg-cyan-800 font-semibold text-slate-50 hover:bg-cyan-950 px-6 py-3 rounded-md">Manage Products</button>
             </div> */}
